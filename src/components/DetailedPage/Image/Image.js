eb919da27d1dbba6ddca8e6afcbac41d9a807c33
TomaszPuscian <pusciant@gmail.com>
2020-09-11 20:58:45 +0200

    photo fix

--- src/components/DetailedPage/Image/Image.js
+++ src/components/DetailedPage/Image/Image.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import examplephoto from './zkXHImtvRUDocKkzkyoTL980AvCTDdSQ.jpg'
 import {DetailedInfoElement} from "../../Styled Components/styled-components";
 
 
@@ -7,7 +6,7 @@ const Image = () => {
 
     return (
         <DetailedInfoElement>
-            <img src={examplephoto} style={{width: 400, height: 400}} alt="example" />
+            <img src={} style={{width: 400, height: 400}} alt="example" />
         </DetailedInfoElement>
     )
 }
